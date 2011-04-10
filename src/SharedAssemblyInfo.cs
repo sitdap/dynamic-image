983270e1dfd08e1989d447741c7dc31cede34211
Tim Jones <github@roastedamoeba.com>
2011-04-10 20:17:23 +0800

    Bumping version number so NuGet packages work

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("sitdap.com")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyFileVersion("1.0.4.0")]
-[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.5.0")]

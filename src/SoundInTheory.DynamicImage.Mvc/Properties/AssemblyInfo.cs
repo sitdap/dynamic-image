03570dd01cd8ac36e7b05df6225dd4df29512b53
Tim Jones <tim@timjones.tw>
2013-04-29 12:50:13 +0800

    Remove SharedAssemblyInfo.cs

--- src/SoundInTheory.DynamicImage.Mvc/Properties/AssemblyInfo.cs
+++ src/SoundInTheory.DynamicImage.Mvc/Properties/AssemblyInfo.cs
@@ -8,5 +8,15 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Extensions for DynamicImage providing integration with ASP.NET MVC 3")]
 [assembly: AssemblyCulture("")]
 
+[assembly: AssemblyConfiguration("Retail")]
+[assembly: AssemblyCompany("Sound in Theory Ltd")]
+[assembly: AssemblyProduct("DynamicImage: Image manipulation library for ASP.NET")]
+[assembly: AssemblyCopyright("Copyright ©2013 Sound in Theory Ltd")]
+[assembly: AssemblyTrademark("sitdap.com")]
+[assembly: ComVisible(false)]
+
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d439e617-debf-44b3-bd17-6859445dc3ac")]
+
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
--- src/SoundInTheory.DynamicImage/Properties/AssemblyInfo.cs
+++ src/SoundInTheory.DynamicImage/Properties/AssemblyInfo.cs
@@ -1,12 +1,19 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Sound In Theory Ltd DynamicImage for ASP.NET")]
 [assembly: AssemblyDescription("DynamicImage is a high-performance image manipulation library for ASP.NET.")]
 [assembly: AssemblyCulture("")]
 
+[assembly: AssemblyConfiguration("Retail")]
+[assembly: AssemblyCompany("Sound in Theory Ltd")]
+[assembly: AssemblyProduct("DynamicImage: Image manipulation library for ASP.NET")]
+[assembly: AssemblyCopyright("Copyright ©2013 Sound in Theory Ltd")]
+[assembly: AssemblyTrademark("sitdap.com")]
+[assembly: ComVisible(false)]
+
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("402d49d1-b195-437a-83d6-3cf53c31bae5")]
+
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]

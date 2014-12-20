16222773751e8f462fef3b69549fc9d039d81f92
Tim Jones <tim@timjones.tw>
2014-12-20 23:46:39 +0800

    Don't use LayerBlenderEffect is BlendMode=Normal for all layers
    As discovered by @geo242, creating a new ImageBrush when compositing
    each layer can result in high memory usage (for compositions with many
    layers), which is completely unnecessary for the most common case where
    BlendMode=Normal for all layers.

--- src/SoundInTheory.DynamicImage/LayerBlender.cs
+++ src/SoundInTheory.DynamicImage/LayerBlender.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using SoundInTheory.DynamicImage.ShaderEffects.LayerBlending;
@@ -18,40 +19,57 @@ namespace SoundInTheory.DynamicImage
 			RenderTargetBitmap rtb = RenderTargetBitmapUtility.CreateRenderTargetBitmap(output.Width, output.Height);
 
 			// Draw background.
-			DrawingContext dc2 = dv.RenderOpen();
+			var dc2 = dv.RenderOpen();
 			dc2.DrawImage(output.InnerBitmap, new System.Windows.Rect(0, 0, output.Width, output.Height));
 			dc2.Close();
 			rtb.Render(dv);
 
-			ImageSource imageSource = output.InnerBitmap;
+		    var layersList = layers.ToList();
 
-			foreach (Layer layer in layers)
-			{
-				using (var effect = new LayerBlenderEffect(layer.BlendMode, output.Width, output.Height)
-				{
-					Background = new ImageBrush(imageSource)
-					{
-						TileMode = TileMode.None,
-						Stretch = Stretch.None,
-						ViewportUnits = BrushMappingMode.RelativeToBoundingBox,
-						Viewport = new System.Windows.Rect(0, 0, 1, 1)
-					}
-				})
-				{
-					dv.Effect = effect;
-					DrawingContext dc = dv.RenderOpen();
-					dc.PushTransform(new TranslateTransform(layer.X + layer.Padding.Left, layer.Y + layer.Padding.Top));
-					dc.DrawImage(layer.Bitmap.InnerBitmap, new System.Windows.Rect(0, 0, layer.Bitmap.Width, layer.Bitmap.Height));
-					dc.Pop();
-					dc.Close();
+            // As an optimisation, if BlendMode=Normal for all layers, don't use LayerBlenderEffect.
+            // Just draw images directly using DrawImage.
+            if (layersList.All(x => x.BlendMode == BlendMode.Normal))
+		    {
+		        foreach (var layer in layersList)
+		            DrawImage(dv, layer, rtb);
+		    }
+		    else
+		    {
+                ImageSource imageSource = output.InnerBitmap;
 
-					rtb.Render(dv);
-				}
+                foreach (var layer in layersList)
+		        {
+		            using (var effect = new LayerBlenderEffect(layer.BlendMode, output.Width, output.Height)
+		            {
+		                Background = new ImageBrush(imageSource)
+		                {
+		                    TileMode = TileMode.None,
+		                    Stretch = Stretch.None,
+		                    ViewportUnits = BrushMappingMode.RelativeToBoundingBox,
+		                    Viewport = new System.Windows.Rect(0, 0, 1, 1)
+		                }
+		            })
+		            {
+		                dv.Effect = effect;
+                        DrawImage(dv, layer, rtb);
+		            }
 
-				imageSource = rtb;
-			}
+		            imageSource = rtb;
+		        }
+		    }
 
-			return new FastBitmap(rtb);
+		    return new FastBitmap(rtb);
 		}
+
+	    private static void DrawImage(DrawingVisual dv, Layer layer, RenderTargetBitmap rtb)
+	    {
+	        var dc = dv.RenderOpen();
+	        dc.PushTransform(new TranslateTransform(layer.X + layer.Padding.Left, layer.Y + layer.Padding.Top));
+	        dc.DrawImage(layer.Bitmap.InnerBitmap, new System.Windows.Rect(0, 0, layer.Bitmap.Width, layer.Bitmap.Height));
+	        dc.Pop();
+	        dc.Close();
+
+	        rtb.Render(dv);
+	    }
 	}
 }
